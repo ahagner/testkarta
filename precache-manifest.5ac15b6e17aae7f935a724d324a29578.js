a3523b301cceeda416d11746f31c4c59079bbf62
Axel Hagner <axelerikhagner@gmail.com>
2019-04-01 16:22:23 +0200

    Updates

---
+++ precache-manifest.5ac15b6e17aae7f935a724d324a29578.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kartan/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fd73e1abf89af123442d",
+    "url": "/kartan/static/js/runtime~main.a033acfa.js"
+  },
+  {
+    "revision": "bdf965eaebe7eb52daf6",
+    "url": "/kartan/static/js/main.08b78392.chunk.js"
+  },
+  {
+    "revision": "3e6e6dfe093712255a17",
+    "url": "/kartan/static/js/2.443f1cf9.chunk.js"
+  },
+  {
+    "revision": "bdf965eaebe7eb52daf6",
+    "url": "/kartan/static/css/main.7c534f31.chunk.css"
+  },
+  {
+    "revision": "5cda6444f29b3d4df92bde421a382e33",
+    "url": "/kartan/index.html"
+  }
+];
